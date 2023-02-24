e7df8c0b58bd19e9c04a80ffed3727512cc6357d
scokljat <selma.cokljat@hotmail.com>
2023-02-24 09:04:38 +0100

    .

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -33,7 +33,7 @@ export async function getStaticPaths() {
   const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
 
   return {
-    fallback: true,
+    fallback: false,
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
